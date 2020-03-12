072b916d60e581e0014983bc52512652819b8a75
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2020-03-12 19:57:20 +0000

    change input circuits to match tutorial

--- input_circuits.py
+++ input_circuits.py
@@ -1,5 +1,6 @@
 import cirq
 from scipy.stats import truncnorm
+import tensorflow_quantum as tfq
 import numpy as np
 
 
@@ -13,16 +14,29 @@ class InputCircuits:
                                      mu_a: float = 0.5, mu_b: float = 0.5, sigma_a: float = 0.01,
                                      sigma_b: float = 0.01):
         a_dist = truncnorm.rvs((0 - mu_a) / sigma_a, (1 - mu_a) / sigma_a, mu_a, sigma_a,
-                               size=int(total_states * prop_a * 2) + 2)
+                               size=total_states)
         b_dist = truncnorm.rvs((0 - mu_b) / sigma_b, (1 - mu_b) / sigma_b, mu_b, sigma_b,
-                               size=int(total_states * (1 - prop_a)) + 2)
-        a_circuits = []
-        b_circuits = []
-        for i in range(int(total_states * prop_a * 2)):
-            a_circuits.append(self.create_a(a_dist[i]))
-        for i in range(int(total_states * (1 - prop_a))):
-            b_circuits.append(self.create_b(b_dist[i]))
-        return a_circuits, b_circuits
+                               size=total_states)
+        circuits = []
+        labels = []
+        for i in range(total_states):
+            label = np.random.choice([0, 1])
+            labels.append(label)
+            if label:
+                circuits.append(self.create_a(a_dist[i]))
+            else:
+                sign = np.random.choice([0,1])
+                circuits.append(self.create_b(b_dist[i], sign))
+
+        split = int(len(circuits) * 0.7)
+        train_circuits = circuits[:split]
+        test_circuits = circuits[split:]
+
+        train_labels = labels[:split]
+        test_labels = labels[split:]
+
+        return tfq.convert_to_tensor(train_circuits), np.array(train_labels), \
+               tfq.convert_to_tensor(test_circuits), np.array(test_labels)
 
     def create_a(self, a: float):
         ops = [cirq.ry(2 * np.arcsin(a))(self.qubits[0])]
@@ -30,13 +44,9 @@ class InputCircuits:
         circuit = cirq.Circuit(ops)
         return circuit
 
-    def create_b(self, b: float):
-        ops1 = [cirq.X(self.qubits[0]),
-                cirq.ISWAP(self.qubits[0], self.qubits[1])**(2 * np.arcsin(b) / np.pi)]
-        ops1.extend([cirq.Z(q) for q in self.qubits[2:]])
-        circuit1 = cirq.Circuit(ops1)
-        ops2 = [cirq.X(self.qubits[0]),
-                cirq.ISWAP(self.qubits[0], self.qubits[1]) ** (- 2 * np.arcsin(b) / np.pi)]
-        ops2.extend(cirq.Z(q) for q in self.qubits[2:])
-        circuit2 = cirq.Circuit(ops2)
-        return circuit1, circuit2
+    def create_b(self, b: float, sign: int):
+        ops = [cirq.X(self.qubits[0]),
+                cirq.ISWAP(self.qubits[0], self.qubits[1])**((-1 ** sign) * 2 * np.arcsin(b) / np.pi)]
+        ops.extend([cirq.Z(q) for q in self.qubits[2:]])
+        circuit = cirq.Circuit(ops)
+        return circuit
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 from encode_state import EncodeState
 from input_circuits import InputCircuits
@@ -7,12 +8,19 @@ from input_circuits import InputCircuits
 def main():
     n = 4
     circuits = InputCircuits(n)
-    a_circuits, b_circuits = circuits.create_discrimination_circuits()
+    train_circuits, train_labels, test_circuits, test_labels = circuits.create_discrimination_circuits()
     encoder = EncodeState(n)
-    encoding_model = encoder.encode_state(a_circuits)
+    encoding_model = encoder.encode_state(train_circuits)
     encoding_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
                            loss=tf.losses.mse)
-    return encoding_model
+    history = encoding_model.fit(x=train_circuits,
+                                 y=train_labels,
+                                 batch_size=10,
+                                 epochs=2,
+                                 verbose=1,
+                                 validation_data=(test_circuits, test_labels))
+    plt.plot(history.history['loss'], label='Training')
+    plt.show()
 
 
 if __name__ == '__main__':
