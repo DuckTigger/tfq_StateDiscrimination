4d20fbbd0f89021cda24c5466d0a517fa8a800ab
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2020-03-23 17:16:29 +0000

    initial commit

--- loss.py
+++ loss.py
@@ -11,17 +11,28 @@ class DiscriminationLoss:
     # @tf.function
     def discrimination_loss(self, y_label, y_measurement):
         y_label = tf.cast(y_label, tf.float32)
-        (error, inconclusive) = tf.map_fn(lambda x: self.measurement_to_loss(x[0], x[1]), (y_label, y_measurement))
+        error, inconclusive = tf.map_fn(lambda x: self.measurement_to_loss(x[0], x[1]), (y_label, y_measurement))
         loss_vec = tf.add(error, inconclusive)
         return tf.reduce_mean(loss_vec)
 
-    def measurement_to_loss(self, label: int, measurement: List):
+    def measurement_to_loss(self, label: int, measurement: tf.Tensor):
         # 1 == a, 0 == b
-        measurement = tf.cast(tf.squeeze(measurement), tf.int32)
-        fn_a = lambda: tf.cast(tf.equal(measurement, [1, -1]), dtype=tf.float32)
-        fn_b = lambda: tf.cast(tf.logical_or(tf.equal(measurement, [1, 1]), tf.equal(measurement, [-1, 1])), dtype=tf.float32)
+        measurement = tf.squeeze(measurement)
+        probs = self.m_outcome_to_probs(measurement)
+        fn_a = lambda: tf.gather(probs, 1)
+        fn_b = lambda: tf.add(tf.gather(probs, 0), tf.gather(probs, 2))
         error = tf.case([(tf.equal(label, 1), fn_a)], default=fn_b)
         error *= self.w_error
-        inconclusive = tf.cast(tf.equal(measurement, [-1, -1]), dtype=tf.float32)
+        inconclusive = tf.gather(probs, 3)
         inconclusive *= self.w_inconclusive
-        return (error, inconclusive)
+        return error, inconclusive
+
+    @staticmethod
+    def m_outcome_to_probs(measurement: tf.Tensor):
+        measurement = tf.divide(tf.add(measurement, 1), 2)
+        prob_0 = lambda x: 1 - x
+        prob_00 = tf.add(prob_0(measurement[0]), prob_0(measurement[1])) / 4
+        prob_01 = tf.add(prob_0(measurement[0]), measurement[1]) / 4
+        prob_10 = tf.add(measurement[0], prob_0(measurement[1])) / 4
+        prob_11 = tf.add(measurement) / 4
+        return tf.stack([prob_00, prob_01, prob_10, prob_11])
