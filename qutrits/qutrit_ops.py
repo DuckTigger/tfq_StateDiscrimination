9f47c4dafaa341249b80125aea316d4302959e5d
Prakash <prakashmurali@gmail.com>
2020-03-31 05:59:08 -0400

    qutrit ops

---
+++ qutrits/qutrit_ops.py
@@ -0,0 +1,86 @@
+import cirq
+import numpy as np
+
+class QutritPlusGate(cirq.SingleQubitGate):
+    def _qid_shape_(self):
+        return (3,)
+
+    def _unitary_(self):
+        return np.array([[0, 0, 1],
+                         [1, 0, 0],
+                         [0, 1, 0]])
+
+    def _circuit_diagram_info_(self, args):
+        return '[+1]'
+
+
+class QutritMinusGate(cirq.SingleQubitGate):
+    def _qid_shape_(self):
+        return (3,)
+
+    def _unitary_(self):
+        return np.array([[0, 1, 0],
+                         [0, 0, 1],
+                         [1, 0, 0]])
+
+    def _circuit_diagram_info_(self, args):
+        return '[-1]'
+
+
+class C2QutritPlusGate(cirq.TwoQubitGate):
+    """
+    2-controlled Qutrit Plus Gate on two qutrits
+    """
+    def _qid_shape_(self):
+        return (3,3,)
+
+    def _unitary_(self):
+        return np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 1, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 1, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 1, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 1, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 1],
+                         [0, 0, 0, 0, 0, 0, 1, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 1, 0]])
+
+    def _circuit_diagram_info_(self, args):
+        return cirq.protocols.CircuitDiagramInfo(
+            wire_symbols=('2', '[+1]'))
+
+class C2QutritQubitXGate(cirq.TwoQubitGate):
+    """
+    2-controlled Qutrit-Qubit CNOT gate
+    """
+    def _qid_shape_(self):
+        return (3,2,)
+
+    def _unitary_(self):
+        return np.array([[1, 0, 0, 0, 0, 0],
+                         [0, 1, 0, 0, 0, 0],
+                         [0, 0, 1, 0, 0, 0],
+                         [0, 0, 0, 1, 0, 0],
+                         [0, 0, 0, 0, 0, 1],
+                         [0, 0, 0, 0, 1, 0]])
+
+    def _circuit_diagram_info_(self, args):
+        return cirq.protocols.CircuitDiagramInfo(
+            wire_symbols=('2', '[X]'))
+
+
+q0 = cirq.LineQid(0, dimension=3)
+q1 = cirq.LineQid(1, dimension=2)
+qubits = [q0, q1]
+circuit = cirq.Circuit(
+    QutritPlusGate().on(q0),
+    QutritPlusGate().on(q0),
+    C2QutritQubitXGate().on(q0, q1),
+    cirq.measure(*qubits, key='result')
+)
+print(circuit)
+
+#circuit.append(cirq.measure(*q0, 'm'))
+simulator = cirq.Simulator()
+result = simulator.run(circuit, repetitions=1)
+print(result)
