e722806be47b2df85f13546f45c729e952fc45eb
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2020-04-22 18:24:12 +0100

    refactor

--- encode_state.py
+++ encode_state.py
@@ -5,7 +5,7 @@ import tensorflow_quantum as tfq
 import tensorflow as tf
 from typing import Iterable, List, Union
 
-from qutrits.qutrit_ops import *
+from model_circuits import ModelCircuits
 
 
 class EncodeState:
@@ -15,98 +15,18 @@ class EncodeState:
         self.n = n_qubits
         self.n_data = n_data_qubits
 
-    def create_encoding_circuit(self, symbols: sp.Symbol = None):
-        if symbols is None:
-            symbols = sp.symbols('enc0:{}'.format(4 * self.n))
-        circuit = cirq.Circuit()
-        circuit.append(cirq.H.on_each(self.qubits))
-        for i, qubit in enumerate(self.qubits):
-            circuit.append(self.one_qubit_unitary(qubit, symbols[3 * i: 3 * i + 3]))
-        for i, (qubit_0, qubit_1) in enumerate(zip(self.qubits, self.qubits[1:] + [self.qubits[0]])):
-            circuit.append(cirq.CNotPowGate(exponent=symbols[3 * self.n + i]).on(qubit_0, qubit_1))
-        circuit.append(cirq.CNOT(self.qubits[3], self.qubits[2]))
-        circuit.append(cirq.measure(self.qubits[2], self.qubits[3], key='m'))
-        return circuit
-
-    def create_layers(self, symbols: Iterable[sp.Symbol], level: int, control_circ: bool = False) -> cirq.Circuit:
-        layer_qubits = self.qubits[:-level] if level else self.qubits
-        control_circ = control_circ if level else False
-        circuit = cirq.Circuit()
-        symbols_0 = sp.symbols(tuple([x.name + '_0' for x in symbols]))
-        symbols_1 = sp.symbols(tuple([x.name + '_1' for x in symbols]))
-        if control_circ:
-            for control, control_sym in enumerate((symbols_0, symbols_1)):
-                circuit.append(self.create_control_layer(control, level, layer_qubits, control_sym))
-        else:
-            circuit.append(self.create_non_control_layer(level, layer_qubits, symbols))
-        circuit.append(cirq.measure(layer_qubits[-1], key='m{}'.format(level)))
-        return circuit
-
-    def create_control_layer(self, control: int, level: int,
-                             layer_qubits: List[cirq.Qid], symbols: sp.Symbol) -> cirq.Circuit:
-        circuit = cirq.Circuit()
-        control_qubit = self.qubits[-level]
-        n = len(layer_qubits)
-        for i, qubit in enumerate(layer_qubits):
-            circuit.append(self.one_qubit_unitary(qubit, symbols[3 * i: 3 * i + 3], control_qubit, control))
-        if not level == len(self.qubits) - 1:
-            for i, (qubit_0, qubit_1) in enumerate(zip(layer_qubits, layer_qubits[1:] + [layer_qubits[0]])):
-                circuit.append(
-                    cirq.CNotPowGate(exponent=symbols[3 * n + i]).on(qubit_0, qubit_1).controlled_by(
-                        control_qubit, control_values=[control]))
-        return circuit
-
-    def create_non_control_layer(self, level: int, layer_qubits: List[cirq.Qid],
-                                 symbols: Union[Iterable[sp.Symbol], sp.Symbol]) -> cirq.Circuit:
-        circuit = cirq.Circuit()
-        n = len(layer_qubits)
-        for i, qubit in enumerate(layer_qubits):
-            circuit.append(self.one_qubit_unitary(qubit, symbols[3 * i: 3 * i + 3]))
-        if not level == len(self.qubits) - 1:
-            for i, (qubit_0, qubit_1) in enumerate(zip(layer_qubits, layer_qubits[1:] + [layer_qubits[0]])):
-                circuit.append(
-                    cirq.CNotPowGate(exponent=symbols[3 * n + i]).on(qubit_0, qubit_1))
-        return circuit
-
-    @staticmethod
-    def one_qubit_unitary(qubit: cirq.Qid, symbols: sp.Symbol, control_qubit: cirq.Qid = None, control: int = None) -> cirq.Circuit:
-        if control_qubit is None:
-            return cirq.Circuit([cirq.X(qubit)**symbols[0],
-                                 cirq.Y(qubit)**symbols[1],
-                                 cirq.Z(qubit)**symbols[2]])
-        else:
-            return cirq.Circuit([cirq.XPowGate(exponent=symbols[0]).on(qubit).controlled_by(
-                                     control_qubit, control_values=[control]),
-                                 cirq.YPowGate(exponent=symbols[1]).on(qubit).controlled_by(
-                                     control_qubit, control_values=[control]),
-                                 cirq.ZPowGate(exponent=symbols[2]).on(qubit).controlled_by(
-                                     control_qubit, control_values=[control])])
-
-    def ent_ops(self):
-        return cirq.Circuit(cirq.CNOT(q1, q2) for q1, q2 in zip(self.qubits, self.qubits[1:] + [self.qubits[0]]))
-
-    @staticmethod
-    def one_qutrit_unitary(qutrit: 'cirq.Qid', symbols: 'sp.Symbol',
-                           control_qutrit: 'cirq.Qid' = None, control: int = None) -> 'cirq.Circuit':
-        if control_qutrit is None:
-            return cirq.Circuit([
-                QutritPlusGate().on
-            ])
-        else:
-            raise NotImplementedError
-
     def discrimination_circuit(self, control_on_measurement: bool = False):
         output = cirq.Circuit()
         for i in range(self.n - self.n_data):
             symbols = sp.symbols('layer{}_0:{}'.format(i, 4 * self.n - i))
-            output.append(self.create_layers(symbols, i, control_on_measurement))
+            output.append(ModelCircuits.create_layers(self.qubits, symbols, i, control_on_measurement))
         return output
 
     def encode_state_PQC(self):
         symbols = sp.symbols('enc0:{}'.format(4 * self.n))
-        encoding_circuit = self.create_encoding_circuit(symbols)
+        encoding_circuit = ModelCircuits.create_encoding_circuit(self.qubits, symbols)
         encoding_input = tf.keras.Input(shape=(), dtype=tf.dtypes.string)
-        encoding_layer = tfq.layers.AddCircuit()(encoding_input, prepend=self.ent_ops())
+        encoding_layer = tfq.layers.AddCircuit()(encoding_input, prepend=ModelCircuits.ent_ops(self.qubits))
         readout_ops = [cirq.Z(self.qubits[2]), cirq.Z(self.qubits[3])]
         encoding_model = tfq.layers.PQC(encoding_circuit, readout_ops)(encoding_layer)
         return tf.keras.Model(inputs=[encoding_input], outputs=[encoding_model])
---
+++ model_circuits.py
@@ -0,0 +1,149 @@
+import cirq
+import sympy as sp
+from typing import List, Iterable, Union
+
+
+class ModelCircuits:
+
+    @staticmethod
+    def create_encoding_circuit(qubits: 'List[cirq.Qid]', symbols: sp.Symbol = None):
+        n = len(qubits)
+        if symbols is None:
+            symbols = sp.symbols('enc0:{}'.format(4 * n))
+        circuit = cirq.Circuit()
+        circuit.append(cirq.H.on_each(qubits))
+        for i, qubit in enumerate(qubits):
+            circuit.append(ModelCircuits.one_qubit_unitary(qubit, symbols[3 * i: 3 * i + 3]))
+        for i, (qubit_0, qubit_1) in enumerate(zip(qubits, qubits[1:] + [qubits[0]])):
+            circuit.append(cirq.CNotPowGate(exponent=symbols[3 * n + i]).on(qubit_0, qubit_1))
+        circuit.append(cirq.CNOT(qubits[3], qubits[2]))
+        circuit.append(cirq.measure(qubits[2], qubits[3], key='m'))
+        return circuit
+
+    @staticmethod
+    def create_layers(qubits: List[cirq.Qid], symbols: Iterable[sp.Symbol], level: int, control_circ: bool = False) -> cirq.Circuit:
+        layer_qubits = qubits[:-level] if level else qubits
+        control_circ = control_circ if level else False
+        circuit = cirq.Circuit()
+        symbols_0 = sp.symbols(tuple([x.name + '_0' for x in symbols]))
+        symbols_1 = sp.symbols(tuple([x.name + '_1' for x in symbols]))
+        if control_circ:
+            for control, control_sym in enumerate((symbols_0, symbols_1)):
+                circuit.append(ModelCircuits.create_control_layer(qubits, control, level, layer_qubits, control_sym))
+        else:
+            final = level == len(qubits) - 1
+            circuit.append(ModelCircuits.create_non_control_layer(final, layer_qubits, symbols))
+        circuit.append(cirq.measure(layer_qubits[-1], key='m{}'.format(level)))
+        return circuit
+
+    @staticmethod
+    def create_leakage_layers(work_qubits: 'List[cirq.Qid]', ancilla_qubits: 'List[cirq.Qid]',
+                              readout_qubits: 'List[cirq.Qid]', symbols: 'Iterable[sp.Symbol]', level: int,
+                              constant_leakage: float = None):
+        (work_qubits, ancilla_qubits, readout_qubits) = \
+            (work_qubits[:-level], ancilla_qubits[:-level], readout_qubits[:-level]) \
+                if level else (work_qubits, ancilla_qubits, readout_qubits)
+
+        circuit = cirq.Circuit()
+
+        symbols_leakage = sp.symbols(tuple([x.name + '_leak' for x in symbols])) if constant_leakage is None\
+            else [constant_leakage for _ in range(len(work_qubits))]
+
+        final = level == len(work_qubits) - 1
+        circuit.append(ModelCircuits.create_non_control_layer(final, work_qubits, symbols))
+        circuit.append(ModelCircuits.leakage_circuit(work_qubits, ancilla_qubits, symbols_leakage))
+        circuit.append(ModelCircuits.OR_circuit(work_qubits, ancilla_qubits, readout_qubits))
+        return circuit
+
+    @staticmethod
+    def create_non_control_layer(final: bool, layer_qubits: List[cirq.Qid],
+                                 symbols: Union[Iterable[sp.Symbol], sp.Symbol]) -> cirq.Circuit:
+        circuit = cirq.Circuit()
+        n = len(layer_qubits)
+        for i, qubit in enumerate(layer_qubits):
+            circuit.append(ModelCircuits.one_qubit_unitary(qubit, symbols[3 * i: 3 * i + 3]))
+        if not final:
+            for i, (qubit_0, qubit_1) in enumerate(zip(layer_qubits, layer_qubits[1:] + [layer_qubits[0]])):
+                circuit.append(
+                    cirq.CNotPowGate(exponent=symbols[3 * n + i]).on(qubit_0, qubit_1))
+        return circuit
+
+    @staticmethod
+    def create_control_layer(qubits: 'List[cirq.Qid]', control: int, level: int,
+                             layer_qubits: List[cirq.Qid], symbols: sp.Symbol) -> cirq.Circuit:
+        circuit = cirq.Circuit()
+        control_qubit = qubits[-level]
+        n = len(layer_qubits)
+        for i, qubit in enumerate(layer_qubits):
+            circuit.append(ModelCircuits.one_qubit_unitary(qubit, symbols[3 * i: 3 * i + 3], control_qubit, control))
+        if not level == len(qubits) - 1:
+            for i, (qubit_0, qubit_1) in enumerate(zip(layer_qubits, layer_qubits[1:] + [layer_qubits[0]])):
+                circuit.append(
+                    cirq.CNotPowGate(exponent=symbols[3 * n + i]).on(qubit_0, qubit_1).controlled_by(
+                        control_qubit, control_values=[control]))
+        return circuit
+
+    @staticmethod
+    def one_qubit_unitary(qubit: cirq.Qid, symbols: sp.Symbol, control_qubit: cirq.Qid = None, control: int = None) -> cirq.Circuit:
+        if control_qubit is None:
+            return cirq.Circuit([cirq.X(qubit)**symbols[0],
+                                 cirq.Y(qubit)**symbols[1],
+                                 cirq.Z(qubit)**symbols[2]])
+        else:
+            return cirq.Circuit([cirq.XPowGate(exponent=symbols[0]).on(qubit).controlled_by(
+                                     control_qubit, control_values=[control]),
+                                 cirq.YPowGate(exponent=symbols[1]).on(qubit).controlled_by(
+                                     control_qubit, control_values=[control]),
+                                 cirq.ZPowGate(exponent=symbols[2]).on(qubit).controlled_by(
+                                     control_qubit, control_values=[control])])
+
+    @staticmethod
+    def ent_ops(qubits: 'List[cirq.Qid]'):
+        return cirq.Circuit(cirq.CNOT(q1, q2) for q1, q2 in zip(qubits, qubits[1:] + [qubits[0]]))
+
+    @staticmethod
+    def leakage(leaky_qubit : 'cirq.Qid', ancilla_qubit: 'cirq.Qid', symbol: 'sp.Symbol'):
+        circuit = cirq.Circuit()
+        circuit.append(cirq.CNOT(control=ancilla_qubit, target=leaky_qubit))
+        circuit.append((cirq.Y ** symbol)(ancilla_qubit))
+        circuit.append(cirq.CNOT(control=leaky_qubit, target=ancilla_qubit))
+        circuit.append(((cirq.Y ** symbol) ** -1)(ancilla_qubit))
+        circuit.append(cirq.CNOT(control=leaky_qubit, target=ancilla_qubit))
+        circuit.append(cirq.CNOT(control=ancilla_qubit, target=leaky_qubit))
+        return circuit
+
+    @staticmethod
+    def leakage_circuit(leaky_qubits: 'List[cirq.Qid]', ancilla_qubits: 'List[cirq.Qid]',
+                        symbols: 'Union[List[sp.Symbol], float]'):
+        circuit = cirq.Circuit()
+        for lq, aq, gamma in zip(leaky_qubits, ancilla_qubits, symbols):
+            circuit += ModelCircuits.leakage(lq, aq, gamma)
+        return circuit
+
+    @staticmethod
+    def quantum_OR(qubit_a: 'cirq.Qid', qubit_b: 'cirq.Qid', output_qubit: 'cirq.Qid'):
+        circuit = cirq.Circuit()
+
+        sqrt_x = cirq.X ** 0.5
+
+        circuit.append(sqrt_x(output_qubit))
+        circuit.append(cirq.CNOT(control=qubit_a, target=output_qubit))
+
+        circuit.append(sqrt_x(output_qubit))
+        circuit.append(cirq.CNOT(control=qubit_b, target=output_qubit))
+
+        circuit.append((sqrt_x ** -1)(output_qubit))
+        circuit.append(cirq.CNOT(control=qubit_a, target=output_qubit))
+
+        circuit.append((sqrt_x ** -1)(output_qubit))
+        circuit.append(cirq.CNOT(control=qubit_b, target=output_qubit))
+
+        circuit.append(cirq.CNOT(control=qubit_a, target=output_qubit))
+        return circuit
+
+    @staticmethod
+    def OR_circuit(a_qubits: 'List[cirq.Qid]', b_qubits: 'List[cirq.Qid]', output_qubits: 'List[cirq.Qid]'):
+        circuit = cirq.Circuit()
+        for a, b, out in zip(a_qubits, b_qubits, output_qubits):
+            circuit += ModelCircuits.quantum_OR(a, b, out)
+        return circuit
