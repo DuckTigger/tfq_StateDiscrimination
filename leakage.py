f7e075f5ad4fee727ad5a5546452062f1b34a294
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2020-04-22 18:25:03 +0100

    add Enricos leakage circuits

--- leakage.py
+++ leakage.py
@@ -2,16 +2,28 @@ import sympy as sp
 import tensorflow_quantum as tfq
 import cirq
 
+from typing import List
+from model_circuits import ModelCircuits
 
-class LeakageCircuit:
 
-    @staticmethod
-    def leakage(leaky_qubit : 'cirq.Qid', ancilla_qubit: 'cirq.Qid', symbol: 'sp.Symbol'):
-        circuit = cirq.Circuit()
-        circuit.append(cirq.CNOT(control=ancilla_qubit, target=leaky_qubit))
-        circuit.append((cirq.Y ** symbol)(ancilla_qubit))
-        circuit.append(cirq.CNOT(control=leaky_qubit, target=ancilla_qubit))
-        circuit.append(((cirq.Y ** symbol) ** -1)(ancilla_qubit))
-        circuit.append(cirq.CNOT(control=leaky_qubit, target=ancilla_qubit))
-        circuit.append(cirq.CNOT(control=ancilla_qubit, target=leaky_qubit))
-        return circuit
+class LeakageModels:
+
+    def __init__(self, n_work_qubits: int, n_data_qubits: int = 2):
+        total_qubits = (3 * n_work_qubits) + n_data_qubits
+        self.qubits = cirq.GridQubit.rect(int(total_qubits / 2), 2)
+        self.readout_qubits = self.qubits[:n_work_qubits]
+        self.ancilla_qubits = self.qubits[n_work_qubits: 2 * n_work_qubits]
+        self.work_qubits = self.qubits[2 * n_work_qubits:3 * n_work_qubits]
+        self.data_qubits = self.qubits[-n_data_qubits:]
+        self.work_data_qubits = self.qubits[2 * n_work_qubits:]
+        self.n = n_work_qubits + n_data_qubits
+
+    def discrimination_circuit(self):
+        output = cirq.Circuit()
+        for i in range(len(self.work_qubits)):
+            disc_symbols = sp.symbols('layer{}_0:{}'.format(i, 4 * self.n - i))
+            leak_symbols = sp.symbols('lekage{}_0:{}'.format(i, self.n - i))
+            output.append(ModelCircuits.create_layers(self.work_data_qubits, disc_symbols, i))
+            output.append()
+
+
