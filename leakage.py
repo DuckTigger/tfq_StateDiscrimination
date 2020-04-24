4b5c228f4ec915adbdc024d865f178c420622326
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2020-04-24 18:20:31 +0100

    fix bugs in training wiht data qubits

--- leakage.py
+++ leakage.py
@@ -1,29 +1,38 @@
 import sympy as sp
 import tensorflow_quantum as tfq
+import tensorflow as tf
 import cirq
-
+from math import ceil
 from typing import List
 from model_circuits import ModelCircuits
 
 
 class LeakageModels:
 
-    def __init__(self, n_work_qubits: int, n_data_qubits: int = 2):
-        total_qubits = (3 * n_work_qubits) + n_data_qubits
-        self.qubits = cirq.GridQubit.rect(int(total_qubits / 2), 2)
-        self.readout_qubits = self.qubits[:n_work_qubits]
-        self.ancilla_qubits = self.qubits[n_work_qubits: 2 * n_work_qubits]
-        self.work_qubits = self.qubits[2 * n_work_qubits:3 * n_work_qubits]
-        self.data_qubits = self.qubits[-n_data_qubits:]
-        self.work_data_qubits = self.qubits[2 * n_work_qubits:]
-        self.n = n_work_qubits + n_data_qubits
+    def __init__(self, n_work_qubits: int, n_data_qubits: int = 2, train_on_data: bool = False,
+                 constant_leakage: float = None):
+        if train_on_data:
+            n_data_qubits = 0
+        self.qubits = cirq.GridQubit.rect(ceil(((3 * n_work_qubits) + n_data_qubits) / 2), 2)
+        self.data_qubits = self.qubits[:n_data_qubits]
+        self.work_qubits = self.qubits[n_data_qubits:n_data_qubits + n_work_qubits]
+        self.ancilla_qubits = self.qubits[n_data_qubits + n_work_qubits: n_data_qubits + 2 * n_work_qubits]
+        self.readout_qubits = self.qubits[n_data_qubits + 2 * n_work_qubits: n_data_qubits + 3 * n_work_qubits]
+        self.n_layers = n_work_qubits
+        self.constant_leakage = constant_leakage
+        self.train_on_data = train_on_data
 
-    def discrimination_circuit(self):
+    def leaky_discrimination_circuit(self):
         output = cirq.Circuit()
-        for i in range(len(self.work_qubits)):
-            disc_symbols = sp.symbols('layer{}_0:{}'.format(i, 4 * self.n - i))
-            leak_symbols = sp.symbols('lekage{}_0:{}'.format(i, self.n - i))
-            output.append(ModelCircuits.create_layers(self.work_data_qubits, disc_symbols, i))
-            output.append()
+        for level in range(self.n_layers):
+            symbols = sp.symbols('layer{}_0:{}'.format(level, 4 * self.n_layers - level))
+            output += ModelCircuits.create_leakage_layers(self.work_qubits, self.ancilla_qubits, self.readout_qubits,
+                                                          symbols, level, self.constant_leakage)
+        return output
 
-
+    def leaky_model(self, backend: 'cirq.Simulator' = None):
+        circuit_input = tf.keras.Input(shape=(), dtype=tf.dtypes.string)
+        leaky_circuit = self.leaky_discrimination_circuit()
+        readout_ops = [cirq.Z(q) for q in self.readout_qubits]
+        leaky_pqc = tfq.layers.PQC(leaky_circuit, readout_ops, backend=backend)(circuit_input)
+        return tf.keras.Model(inputs=[circuit_input], outputs=[leaky_pqc])
--- tests/test_leakage.py
+++ tests/test_leakage.py
@@ -1,9 +1,221 @@
 import unittest
+import numpy as np
+import sympy as sp
+import cirq
+import tensorflow_quantum as tfq
+
+from model_circuits import ModelCircuits
+from leakage import LeakageModels
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class TestLeakageCircuits(unittest.TestCase):
+    def test_print_circuit(self):
+        n_work = 2
+        qubits = cirq.LineQubit.range(3 * n_work)
+        work = qubits[:n_work]
+        ancilla = qubits[n_work:2*n_work]
+        readout = qubits[2*n_work:3*n_work]
+
+        circuit = cirq.Circuit()
+        for level in range(len(work)):
+            symbols = sp.symbols('layer{}_0:{}'.format(level, 4 * n_work - level))
+            circuit += ModelCircuits.create_leakage_layers(work, ancilla, readout, symbols, level)
+        hard_coded = cirq.Circuit([
+                cirq.Moment(operations=[
+                (cirq.X ** sp.Symbol('layer0_0')).on(cirq.LineQubit(0)),
+                (cirq.X ** sp.Symbol('layer0_3')).on(cirq.LineQubit(1)),
+                (cirq.X ** 0.5).on(cirq.LineQubit(5)),
+                (cirq.X ** 0.5).on(cirq.LineQubit(4)),
+            ]), cirq.Moment(operations=[
+                (cirq.Y ** sp.Symbol('layer0_1')).on(cirq.LineQubit(0)),
+                (cirq.Y ** sp.Symbol('layer0_4')).on(cirq.LineQubit(1)),
+            ]), cirq.Moment(operations=[
+                (cirq.Z ** sp.Symbol('layer0_2')).on(cirq.LineQubit(0)),
+                (cirq.Z ** sp.Symbol('layer0_5')).on(cirq.LineQubit(1)),
+            ]), cirq.Moment(operations=[
+                (cirq.CNOT ** sp.Symbol('layer0_6')).on(cirq.LineQubit(0), cirq.LineQubit(1)),
+            ]), cirq.Moment(operations=[
+                (cirq.CNOT ** sp.Symbol('layer0_7')).on(cirq.LineQubit(1), cirq.LineQubit(0)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(2), cirq.LineQubit(0)),
+                cirq.CNOT.on(cirq.LineQubit(3), cirq.LineQubit(1)),
+            ]), cirq.Moment(operations=[
+                (cirq.Y ** sp.Symbol('layer0_0_leak')).on(cirq.LineQubit(2)),
+                (cirq.Y ** sp.Symbol('layer0_1_leak')).on(cirq.LineQubit(3)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(0), cirq.LineQubit(2)),
+                cirq.CNOT.on(cirq.LineQubit(1), cirq.LineQubit(3)),
+            ]), cirq.Moment(operations=[
+                (cirq.Y ** sp.Mul(sp.Integer(-1), sp.Symbol('layer0_0_leak'))).on(cirq.LineQubit(2)),
+                (cirq.Y ** sp.Mul(sp.Integer(-1), sp.Symbol('layer0_1_leak'))).on(cirq.LineQubit(3)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(0), cirq.LineQubit(2)),
+                cirq.CNOT.on(cirq.LineQubit(1), cirq.LineQubit(3)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(2), cirq.LineQubit(0)),
+                cirq.CNOT.on(cirq.LineQubit(3), cirq.LineQubit(1)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(1), cirq.LineQubit(5)),
+            ]), cirq.Moment(operations=[
+                (cirq.X ** 0.5).on(cirq.LineQubit(5)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(3), cirq.LineQubit(5)),
+            ]), cirq.Moment(operations=[
+                (cirq.X ** -0.5).on(cirq.LineQubit(5)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(1), cirq.LineQubit(5)),
+            ]), cirq.Moment(operations=[
+                (cirq.X ** -0.5).on(cirq.LineQubit(5)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(3), cirq.LineQubit(5)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(1), cirq.LineQubit(5)),
+            ]), cirq.Moment(operations=[
+                cirq.MeasurementGate(1, 'm0', ()).on(cirq.LineQubit(5)),
+            ]), cirq.Moment(operations=[
+                (cirq.X ** sp.Symbol('layer1_0')).on(cirq.LineQubit(0)),
+            ]), cirq.Moment(operations=[
+                (cirq.Y ** sp.Symbol('layer1_1')).on(cirq.LineQubit(0)),
+            ]), cirq.Moment(operations=[
+                (cirq.Z ** sp.Symbol('layer1_2')).on(cirq.LineQubit(0)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(2), cirq.LineQubit(0)),
+            ]), cirq.Moment(operations=[
+                (cirq.Y ** sp.Symbol('layer1_0_leak')).on(cirq.LineQubit(2)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(0), cirq.LineQubit(2)),
+            ]), cirq.Moment(operations=[
+                (cirq.Y ** sp.Mul(sp.Integer(-1), sp.Symbol('layer1_0_leak'))).on(cirq.LineQubit(2)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(0), cirq.LineQubit(2)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(2), cirq.LineQubit(0)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(0), cirq.LineQubit(4)),
+            ]), cirq.Moment(operations=[
+                (cirq.X ** 0.5).on(cirq.LineQubit(4)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(2), cirq.LineQubit(4)),
+            ]), cirq.Moment(operations=[
+                (cirq.X ** -0.5).on(cirq.LineQubit(4)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(0), cirq.LineQubit(4)),
+            ]), cirq.Moment(operations=[
+                (cirq.X ** -0.5).on(cirq.LineQubit(4)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(2), cirq.LineQubit(4)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.LineQubit(0), cirq.LineQubit(4)),
+            ]), cirq.Moment(operations=[
+                cirq.MeasurementGate(1, 'm1', ()).on(cirq.LineQubit(4)),
+            ])
+            ])
+        cirq.testing.assert_same_circuits(circuit, hard_coded)
+
+    def test_qubits_distinct(self):
+        for n in range(1, 6):
+            model = LeakageModels(n, n, False)
+            data_set = set(model.data_qubits)
+            ancilla_set = set(model.ancilla_qubits)
+            readout_set = set(model.readout_qubits)
+            self.assertEqual(len(data_set.difference(ancilla_set)), len(data_set))
+            self.assertEqual(len(data_set.difference(readout_set)), len(data_set))
+            self.assertEqual(len(ancilla_set.difference(readout_set)), len(ancilla_set))
+
+    def test_leakage_model_no_data_train(self):
+        model = LeakageModels(2, 2, False)
+        circuit = model.leaky_discrimination_circuit()
+        hard_coded = cirq.Circuit([
+            cirq.Moment(operations=[
+                (cirq.X ** sp.Symbol('layer0_0')).on(cirq.GridQubit(1, 0)),
+                (cirq.X ** sp.Symbol('layer0_3')).on(cirq.GridQubit(1, 1)),
+                (cirq.X ** 0.5).on(cirq.GridQubit(3, 1)),
+                (cirq.X ** 0.5).on(cirq.GridQubit(3, 0)),
+            ]), cirq.Moment(operations=[
+                (cirq.Y ** sp.Symbol('layer0_1')).on(cirq.GridQubit(1, 0)),
+                (cirq.Y ** sp.Symbol('layer0_4')).on(cirq.GridQubit(1, 1)),
+            ]), cirq.Moment(operations=[
+                (cirq.Z ** sp.Symbol('layer0_2')).on(cirq.GridQubit(1, 0)),
+                (cirq.Z ** sp.Symbol('layer0_5')).on(cirq.GridQubit(1, 1)),
+            ]), cirq.Moment(operations=[
+                (cirq.CNOT ** sp.Symbol('layer0_6')).on(cirq.GridQubit(1, 0), cirq.GridQubit(1, 1)),
+            ]), cirq.Moment(operations=[
+                (cirq.CNOT ** sp.Symbol('layer0_7')).on(cirq.GridQubit(1, 1), cirq.GridQubit(1, 0)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(2, 0), cirq.GridQubit(1, 0)),
+                cirq.CNOT.on(cirq.GridQubit(2, 1), cirq.GridQubit(1, 1)),
+            ]), cirq.Moment(operations=[
+                (cirq.Y ** sp.Symbol('layer0_0_leak')).on(cirq.GridQubit(2, 0)),
+                (cirq.Y ** sp.Symbol('layer0_1_leak')).on(cirq.GridQubit(2, 1)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(1, 0), cirq.GridQubit(2, 0)),
+                cirq.CNOT.on(cirq.GridQubit(1, 1), cirq.GridQubit(2, 1)),
+            ]), cirq.Moment(operations=[
+                (cirq.Y ** sp.Mul(sp.Integer(-1), sp.Symbol('layer0_0_leak'))).on(cirq.GridQubit(2, 0)),
+                (cirq.Y ** sp.Mul(sp.Integer(-1), sp.Symbol('layer0_1_leak'))).on(cirq.GridQubit(2, 1)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(1, 0), cirq.GridQubit(2, 0)),
+                cirq.CNOT.on(cirq.GridQubit(1, 1), cirq.GridQubit(2, 1)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(2, 0), cirq.GridQubit(1, 0)),
+                cirq.CNOT.on(cirq.GridQubit(2, 1), cirq.GridQubit(1, 1)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(1, 1), cirq.GridQubit(3, 1)),
+            ]), cirq.Moment(operations=[
+                (cirq.X ** 0.5).on(cirq.GridQubit(3, 1)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(2, 1), cirq.GridQubit(3, 1)),
+            ]), cirq.Moment(operations=[
+                (cirq.X ** -0.5).on(cirq.GridQubit(3, 1)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(1, 1), cirq.GridQubit(3, 1)),
+            ]), cirq.Moment(operations=[
+                (cirq.X ** -0.5).on(cirq.GridQubit(3, 1)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(2, 1), cirq.GridQubit(3, 1)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(1, 1), cirq.GridQubit(3, 1)),
+            ]), cirq.Moment(operations=[
+                cirq.MeasurementGate(1, 'm0', ()).on(cirq.GridQubit(3, 1)),
+            ]), cirq.Moment(operations=[
+                (cirq.X ** sp.Symbol('layer1_0')).on(cirq.GridQubit(1, 0)),
+            ]), cirq.Moment(operations=[
+                (cirq.Y ** sp.Symbol('layer1_1')).on(cirq.GridQubit(1, 0)),
+            ]), cirq.Moment(operations=[
+                (cirq.Z ** sp.Symbol('layer1_2')).on(cirq.GridQubit(1, 0)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(2, 0), cirq.GridQubit(1, 0)),
+            ]), cirq.Moment(operations=[
+                (cirq.Y ** sp.Symbol('layer1_0_leak')).on(cirq.GridQubit(2, 0)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(1, 0), cirq.GridQubit(2, 0)),
+            ]), cirq.Moment(operations=[
+                (cirq.Y ** sp.Mul(sp.Integer(-1), sp.Symbol('layer1_0_leak'))).on(cirq.GridQubit(2, 0)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(1, 0), cirq.GridQubit(2, 0)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(2, 0), cirq.GridQubit(1, 0)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(1, 0), cirq.GridQubit(3, 0)),
+            ]), cirq.Moment(operations=[
+                (cirq.X ** 0.5).on(cirq.GridQubit(3, 0)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(2, 0), cirq.GridQubit(3, 0)),
+            ]), cirq.Moment(operations=[
+                (cirq.X ** -0.5).on(cirq.GridQubit(3, 0)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(1, 0), cirq.GridQubit(3, 0)),
+            ]), cirq.Moment(operations=[
+                (cirq.X ** -0.5).on(cirq.GridQubit(3, 0)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(2, 0), cirq.GridQubit(3, 0)),
+            ]), cirq.Moment(operations=[
+                cirq.CNOT.on(cirq.GridQubit(1, 0), cirq.GridQubit(3, 0)),
+            ]), cirq.Moment(operations=[
+                cirq.MeasurementGate(1, 'm1', ()).on(cirq.GridQubit(3, 0)),
+            ])
+            ])
+        cirq.testing.assert_same_circuits(circuit, hard_coded)
 
 
 if __name__ == '__main__':
